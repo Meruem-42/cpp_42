ce3dff1add5f13ca86b41ea6ba0e601426aa2340
Alexandre Herlaud <aherlaud@student.42.fr>
2025-08-19 14:17:22 +0200

    up to date with 42 git repo

--- cpp00/ex01/src/add_contact.cpp
+++ cpp00/ex01/src/add_contact.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   add_contact.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:08:31 by meruem            #+#    #+#             */
-/*   Updated: 2025/08/13 18:39:27 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/17 18:18:27 by meruem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,22 @@ void Contact::fill_data_contact()
 {
     std::cout << "First Name : ";
     getline(std::cin, first_name);
-    protection_cin();
+    if (protection_cin() == 1)
+        return ;
     std::cout << "Last Name : ";
     getline(std::cin, last_name);
-    protection_cin();
+    if (protection_cin() == 1)
+        return ;
     std::cout << "Nickname : ";
     getline(std::cin, nickname);
-    protection_cin();
+    if (protection_cin() == 1)
+        return ;
     std::cout << "Phone Number : ";
     getline(std::cin, phone_number);
-    protection_cin();
+    if (protection_cin() == 1)
+        return ;
     std::cout << "Darkest Secret : ";
     getline(std::cin, darkest_secret);
-    protection_cin();
 }
 
 void Phonebook::add_contact()
--- cpp00/ex01/src/error_management.cpp
+++ cpp00/ex01/src/error_management.cpp
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   error_management.cpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:08:40 by meruem            #+#    #+#             */
-/*   Updated: 2025/08/13 18:41:44 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/17 18:15:23 by meruem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/main.hpp"
 
-void protection_cin()
+int protection_cin()
 {
     if (std::cin.eof())
     {
         std::cout << std::endl;
         std::cout << "error : end of file cin need to exit" << std::endl;
-        exit(1);
+        return (1);
     }
+    return (0);
 }
