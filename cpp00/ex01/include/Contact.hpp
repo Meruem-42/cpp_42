384c3dfff1f92577b1c52c7ec5526c5c31ed951a
Alexandre Herlaud <aherlaud@student.42.fr>
2025-08-19 19:27:41 +0200

    clean : clean and fix all nornm issue like file names and purpose of cpp files name and hpp

---
+++ cpp00/ex01/include/Contact.hpp
@@ -0,0 +1,29 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+
+#include "global.hpp"
+
+class Contact
+{
+private:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+
+public:
+    void set_contact();
+    std::string get_first_name();
+    std::string get_last_name();
+    std::string get_nickname();
+    std::string get_phone_number();
+    std::string get_darkest_secret();
+};
+
+void display_line_contact(int index, Contact c);
+void display_contact(Contact c);
+
+#endif
---
+++ cpp00/ex01/include/global.hpp
@@ -0,0 +1,12 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+int protection_cin();
+int is_empty(std::string field);
+
+#endif
