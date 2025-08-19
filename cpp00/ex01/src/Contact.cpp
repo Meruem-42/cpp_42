c8ba227b1413365613560cc03d68ee0c4f772157
Meruem <alex.herlaud99@gmail.com>
2025-08-19 22:54:35 +0200

    fix : add in loop the protection for getline contact for not infinite loops

--- cpp00/ex01/src/Contact.cpp
+++ cpp00/ex01/src/Contact.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 16:42:55 by aherlaud          #+#    #+#             */
-/*   Updated: 2025/08/19 17:01:56 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/19 22:50:27 by meruem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,37 @@ void Contact::set_contact()
     {
         std::cout << "First Name : ";
         getline(std::cin, first_name);
+        if (protection_cin() == 1)
+            return;
     }
-    if (protection_cin() == 1)
-        return;
     while (last_name.length() == 0 || is_empty(last_name) == 1)
     {
         std::cout << "Last Name : ";
         getline(std::cin, last_name);
+        if (protection_cin() == 1)
+            return;
     }
-    if (protection_cin() == 1)
-        return;
     while (nickname.length() == 0 || is_empty(nickname) == 1)
     {
         std::cout << "Nickame : ";
         getline(std::cin, nickname);
+        if (protection_cin() == 1)
+            return;
     }
-    if (protection_cin() == 1)
-        return;
+
     while (phone_number.length() == 0 || is_empty(phone_number) == 1)
     {
         std::cout << "Phone number : ";
         getline(std::cin, phone_number);
+        if (protection_cin() == 1)
+            return;
     }
-    if (protection_cin() == 1)
-        return;
     while (darkest_secret.length() == 0 || is_empty(darkest_secret) == 1)
     {
         std::cout << "Darkest Secret : ";
         getline(std::cin, darkest_secret);
+        if (protection_cin() == 1)
+            return;
     }
 }
 
