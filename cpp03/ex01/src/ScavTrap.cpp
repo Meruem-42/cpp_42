f7ae2875dd8a80bfb2bf2ec7a14d008f4285794d
Meruem <alex.herlaud99@gmail.com>
2025-08-20 21:57:15 +0200

    cpp03 ex01 : finished with start of test see if more needed

--- cpp03/ex01/src/ScavTrap.cpp
+++ cpp03/ex01/src/ScavTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 16:26:17 by aherlaud          #+#    #+#             */
-/*   Updated: 2025/08/20 20:09:12 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/20 21:55:12 by meruem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void ScavTrap::attack(const std::string &target)
 {
     if (energy_points > 0 && hit_points > 0)
     {
-        std::cout << "ScavTrap " << name << " attacks " << target << ", causing " << attack_damage << " points of damage !" << std::endl;
+        std::cout << "ScavTrap " << name << " attacks " << target << ", causing " << attack_damage << " points of damage ! Energy left: " << energy_points << std::endl;
         --energy_points;
     }
     else
