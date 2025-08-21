8d0d67ffad0a455f7289374f38641f63154198fe
Alexandre Herlaud <aherlaud@f6r6s3.clusters.42paris.fr>
2025-08-21 17:54:42 +0200

    cpp03 ex01 : add the guardGate mode and add a test for it in main

--- cpp03/ex01/src/ScavTrap.cpp
+++ cpp03/ex01/src/ScavTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 16:26:17 by aherlaud          #+#    #+#             */
-/*   Updated: 2025/08/20 21:55:12 by meruem           ###   ########.fr       */
+/*   Updated: 2025/08/21 17:43:45 by aherlaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ void ScavTrap::attack(const std::string &target)
 {
     if (energy_points > 0 && hit_points > 0)
     {
-        std::cout << "ScavTrap " << name << " attacks " << target << ", causing " << attack_damage << " points of damage ! Energy left: " << energy_points << std::endl;
+        std::cout << "ScavTrap " << name << " attacks " << target << ", causing " << attack_damage << " points of damage !";
         --energy_points;
+        std::cout << " Energy left: " << energy_points << std::endl;
     }
     else
         std::cout << "ScavTrap " << name << " cant attack check HP(" << hit_points << ") or HE(" << energy_points << ") too low" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << name << " is in Gate Keeper mode" << std::endl;
 }
