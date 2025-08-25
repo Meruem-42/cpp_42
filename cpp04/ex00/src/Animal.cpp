db764bb17f061f03d23235f56726d2206abe3c9f
Alexandre Herlaud <aherlaud@f6r6s6.clusters.42paris.fr>
2025-08-25 16:31:51 +0200

    cpp04 ex00 : fix little bugs and clean

--- cpp04/ex00/src/Animal.cpp
+++ cpp04/ex00/src/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:04:29 by aherlaud          #+#    #+#             */
-/*   Updated: 2025/08/23 16:49:15 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/25 16:12:42 by aherlaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,4 @@ std::string Animal::getType() const
 
 void Animal::makeSound() const
 {
-    if (type == "Cat")
-        std::cout << "cats don't bark" << std::endl;
-    if (type == "Dog")
-        std::cout << "dogs bark" << std::endl;
 }
