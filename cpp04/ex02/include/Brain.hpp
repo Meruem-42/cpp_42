b4aa68209259abff6554350fb1813752c153b1bb
Meruem <alex.herlaud99@gmail.com>
2025-08-25 23:27:13 +0200

    cpp04 ex02 : finihsed exercize on abstract classes

---
+++ cpp04/ex02/include/Brain.hpp
@@ -0,0 +1,32 @@
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+
+class Brain
+{
+private:
+    std::string ideas[100];
+
+public:
+    Brain() { std::cout << "Brain default constructor" << std::endl; };
+    Brain(const Brain &other)
+    {
+        std::cout << "Brain copy constructor" << std::endl;
+        for (int i = 0; i < 100; ++i)
+            ideas[i] = other.ideas[i];
+    };
+    Brain &operator=(const Brain &other)
+    {
+        std::cout << "Brain operator constructor" << std::endl;
+        if (this != &other)
+        {
+            for (int i = 0; i < 100; ++i)
+                this->ideas[i] = other.ideas[i];
+        }
+        return (*this);
+    }
+    ~Brain() { std::cout << "Brain Destructor is called" << std::endl; };
+};
+
+#endif
---
+++ cpp04/ex02/include/Dog.hpp
@@ -0,0 +1,46 @@
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include <iostream>
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal
+{
+private:
+    Brain *brain;
+
+public:
+    Dog()
+    {
+        std::cout << "Dog default constructor" << std::endl;
+        type = "Dog";
+        brain = new Brain();
+    };
+    Dog(const Dog &other)
+        : Animal(other)
+    { 
+        std::cout << "Dog copy constructor" << std::endl;
+        brain = new Brain(*(other.brain));
+    };
+    Dog &operator=(const Dog &other)
+    {
+        std::cout << "Dog operator constructor" << std::endl;
+        if (this != &other)
+        {
+            Animal::operator=(other);
+            brain = new Brain(*(other.brain));
+        }
+        return (*this);
+    }
+    ~Dog()
+    {
+        delete brain;
+        std::cout << "Dog Destructor is called" << std::endl;
+    };
+
+    void makeSound() const ;
+};
+
+#endif
