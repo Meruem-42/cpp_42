1f2663ea1611b9161e607621cae5596bed5c4c88
Alexandre Herlaud <aherlaud@f6r4s4.clusters.42paris.fr>
2025-10-01 18:43:18 +0200

    new feature (CPP05 ex02) : initialize all classes

---
+++ cpp05/ex02/include/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+class AForm;
+
+class Bureaucrat
+{
+private:
+    const std::string name;
+    int grade;
+public:
+    Bureaucrat(std::string n, int g);
+    Bureaucrat(const Bureaucrat& other);
+    Bureaucrat &operator=(const Bureaucrat &other);
+    ~Bureaucrat();
+
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+    
+    std::string getName() const;
+    int getGrade() const;
+    
+    void incrementGrade();
+    void decrementGrade();
+
+    void signForm(AForm& c);
+};
+
+std::ostream &operator<<(std::ostream &cout, const Bureaucrat &b);
+
+#endif
---
+++ cpp05/ex02/include/ShrubberyCreationForm.hpp
@@ -0,0 +1,16 @@
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+#include "../include/AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+private:
+    /* data */
+public:
+    ShrubberyCreationForm(/* args */);
+    ~ShrubberyCreationForm();
+};
+
+
+#endif
