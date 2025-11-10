9585bd25afbc0714b262824f4f9b34c22f67816b
Meruem <alex.herlaud99@gmail.com>
2025-11-10 20:43:09 +0100

    neww feature(CPP08) : ex01 finish most implementation with test main for basic case. need to add the addnummber with range and more edge case

--- cpp08/ex01/src/Span.cpp
+++ cpp08/ex01/src/Span.cpp
@@ -1,7 +1,7 @@
 #include "../include/Span.hpp"
 
 Span::Span()
-    : array_(nullptr), N_(0), fill_number_(0)
+    : array_(NULL), N_(0), fill_number_(0)
 {
 }
 
@@ -35,7 +35,62 @@ Span::~Span()
     delete array_;
 }
 
-const char *Span::IsFull::why() const
+void Span::addNumber(int num)
+{
+    if(fill_number_ == N_)
+        throw IsFull();
+    array_[fill_number_] = num;
+    ++fill_number_;
+}
+
+int Span::shortestSpan() const
+{
+    if(fill_number_ < 2)
+        throw NotEnoughElement();
+    int short_span = 0;
+    for(unsigned int i = 0; i < N_; ++i)
+        for(unsigned int j = i + 1; j < N_; ++j)
+        {
+            if(i == 0)
+                short_span = abs(array_[i] - array_[j]);
+            if (abs(array_[i] - array_[j]) < short_span)
+                short_span = abs(array_[i] - array_[j]);
+        }
+    return short_span;
+}
+
+int Span::longestSpan() const
+{
+
+    if(fill_number_ < 2)
+        throw NotEnoughElement();
+    int max;
+    int min;
+    for(unsigned int i = 0; i < N_; ++i) {
+        if(i == 0)
+            max = array_[i];
+        if(array_[i] > max)
+            max = array_[i];
+    }
+    for(unsigned int i = 0; i < N_; ++i) {
+        if(i == 0)
+            min = array_[i];
+        if(array_[i] < min)
+            min = array_[i];
+    }
+
+    return max - min;
+}
+
+
+const char *Span::IsFull::why() const throw()
 {
     return ("Span is full");
 }
+
+const char *Span::NotEnoughElement::why() const throw()
+{
+    return ("array need to have more than 1 element");
+}
+
+
