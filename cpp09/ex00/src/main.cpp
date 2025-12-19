a2a8028f4c745a8f7e1bfc515ef93e46eefced43
Alexandre Herlaud <aherlaud@student.42.fr>
2025-12-19 19:43:11 +0100

    new feature (store map & show results) : add functions to fill the map with Date class as key and price as value, then read the input file and perform print and calculus. Need to handle input file errors and clean

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -1,9 +1,34 @@
 #include "../include/BitcoinExchange.hpp"
 
+void parse_input(BitcoinExchange test, const char* file_name)
+{
+    std::ifstream file(file_name);
+    // if (!file.is_open()) {
+    // throw std::runtime_error("Error: could not open file.");
+    // }
+    std::string line;
+    std::getline(file, line);
+    // verify_header(line);
+    while(std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        std::string item;
+        std::getline(ss, item, '|');
+        Date date = getDate(item);
+        float price = test.search_value(date);
+        std::getline(ss, item);
+        float number = getNumber(item);
+        std::cout << date.to_string() << " => " << number << " = " << number*price << std::endl;
+    }
+}
+
 int main(int ac, char **av)
 {
     (void)ac;
-    BitcoinExchange test(av[1]);
-    Date date("2014-11-12");
-    std::cout << date.to_string() << std::endl;
+    (void)av;
+    BitcoinExchange test("./data.csv");
+    Date date("2022-2-11");
+    parse_input(test, av[1]);
+    // std::cout << test.search_value(date) << std::endl;
+    // std::cout << date.to_string() << std::endl;
 }
