0d21cf78db8fa8f7a9e7fb6980b532b8e80f93d5
Alexandre Herlaud <aherlaud@student.42.fr>
2026-01-02 15:30:19 +0100

    fix&clean (cpp08 ex00) : add some tests in the main with onother container and move the template function easyfind in the tpp file

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -12,34 +12,36 @@
 
 #include "../include/easyfind.hpp"
 #include <vector>
+#include <deque>
 
 int main()
 {
-    std::vector<int> v;
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
-    v.push_back(50);
+    std::vector<int> test1;
+    test1.push_back(10);
+    test1.push_back(20);
+    std::deque<int> test2;
+    test2.push_back(30);
+    test2.push_back(50);
 
     std::cout << "TEST NUMBER FOUND" << "\n\n";
     try
     {
-        easyfind(v, 20);
+        easyfind(test1, 20);
         std::cout << "number found" << std::endl;
     }
     catch (const NotFound &e)
     {
-        std::cout << "number not found" << std::endl;
+        std::cout << e.what() << std::endl;
     }
 
     std::cout << "\nTEST NUMBER NOT FOUND" << "\n\n";
     try
     {
-        easyfind(v, 40);
+        easyfind(test2, 20);
         std::cout << "number found" << std::endl;
     }
     catch (const NotFound &e)
     {
-        std::cout << "number not found" << std::endl;
+        std::cout << e.what() << std::endl;
     }
 }
