2514a306dbe6ada6773c87a8ffd4f4f1777da773
Alexandre Herlaud <aherlaud@student.42.fr>
2026-01-09 19:49:10 +0100

    new feature (cpp09 ex02) : finish the constructors of the class PmergeMe and start testing (need more around copys and operator=)

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -1,4 +1,36 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+
+class PmergeMe
+{
+private:
+    std::vector<int> vect_;
+    std::deque<int>  deq_;
+
+    void sortVector();
+    void sortDeque();
+
+public:
+    PmergeMe(char **av);
+    PmergeMe(const PmergeMe &other);
+    PmergeMe &operator=(const PmergeMe &other);
+    ~PmergeMe();
+
+    class ErrorPmergeMe : public std::exception
+    {
+    public :
+        const char *what() const throw();
+    };
+
+    void sort();
+};
+
+
+
 #endif
