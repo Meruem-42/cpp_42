1bd63caccc52e3e7c6886c345dbfa9c47cf305ac
Alexandre Herlaud <aherlaud@f6r5s3.clusters.42paris.fr>
2026-01-09 19:49:10 +0100

    new feature (cpp09 ex02) : finish the constructors of the class PmergeMe and start testing (need more around copys and operator=)

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -1,1 +1,49 @@
 #include "../include/PmergeMe.hpp"
+
+PmergeMe::PmergeMe(char **av)
+{
+    int i = 1;
+    while(av[i] != NULL)
+    {
+        std::string input = av[i];
+        std::stringstream ss(input);
+        std::string line = "";
+        std::getline(ss, line);
+        // std::cout << line[0] << std::endl;
+        if (isdigit(line[0]))
+        {
+            vect_.push_back(atoi(line.c_str()));
+            deq_.push_back(atoi(line.c_str()));
+        }
+        else
+            throw ErrorPmergeMe();
+        ++i;
+    }
+    std::cout << *vect_.begin() << std::endl;
+    std::cout << *deq_.begin() << std::endl;
+    std::cout << *(--vect_.end()) << std::endl;
+    std::cout << *(--deq_.end()) << std::endl;
+}
+
+PmergeMe::PmergeMe(const PmergeMe &other)
+    : vect_(other.vect_), deq_(other.deq_) 
+{
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    vect_ = other.vect_;
+    deq_ = other.deq_;
+    return (*this);
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+const char *PmergeMe::ErrorPmergeMe::what() const throw()
+{
+    return ("Error");
+}
+
+
