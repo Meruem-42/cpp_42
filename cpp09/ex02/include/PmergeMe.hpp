93838f6a9ec143090190472a96c6173e9c64d4aa
Meruem <alex.herlaud99@gmail.com>
2026-01-12 00:13:22 +0100

    new feature (cpp09 ex02) : finished algirthm for vector sort with ford johnson need to test more and simplify the syntax

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <cmath>
 
 class PmergeMe
 {
@@ -13,11 +14,13 @@ private:
     std::vector<int> vect_;
     std::deque<int>  deq_;
 
-    void sortVector();
+
+    void sortVector(std::vector<int> &winners);
     void sortDeque();
 
 public:
-    PmergeMe(char **av);
+    PmergeMe();
+    // PmergeMe(char **av);
     PmergeMe(const PmergeMe &other);
     PmergeMe &operator=(const PmergeMe &other);
     ~PmergeMe();
@@ -27,10 +30,21 @@ public:
     public :
         const char *what() const throw();
     };
-
-    void sort();
+    void print_containers_class();
+    void binary_search(std::vector<std::pair<int, int> > labels);
+    void sort(char **av);
 };
 
+void print_containers(std::vector<int> &vect, std::deque<int> &deq);
 
+template <typename C>
+void print_container(const C& container)
+{
+    std::cout << "container values :" << std::endl;
+    for (typename C::const_iterator it = container.begin(); it != container.end(); ++it)
+    {
+        std::cout << "\t" << *it << std::endl;
+    }
+}
 
 #endif
