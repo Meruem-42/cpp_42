1efda9526eec9391e1ab39909d50b470fa4f240d
Meruem <alex.herlaud99@gmail.com>
2026-01-12 22:15:59 +0100

    new feature (cpp09 ex02) : finished exercize with vect and deq copy algoritm, printing the result of the algo with the time for each. Error management for sorting error() and bad input (too much and not egough, negative, int_max)

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -4,11 +4,18 @@
 
 int main(int ac, char **av)
 {
-    if (ac < 2)
+    if (ac < 2 || ac > 5000)
     {
         std::cout << "Error" << std::endl;
         return (1);
     }
     PmergeMe test;
-    test.sort(av);
+    try
+    {
+        test.sort(av);
+    }
+    catch(PmergeMe::SortingError& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
